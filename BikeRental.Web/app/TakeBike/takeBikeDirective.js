f5022725d22cfc8955c2f8d2036dcd0ed9318175
Kirichenko-sanek <alexander.kirichenkoo@gmail.com>
2016-10-11 22:38:55 +0300

    Delete jquery in html

---
+++ BikeRental.Web/app/TakeBike/takeBikeDirective.js
@@ -0,0 +1,17 @@
+﻿(function (app) {
+    app.directive('datetimepicker',function() {
+        return {
+            restrict: 'C',
+            require: 'ngModel',
+            link: function (scope, element) {
+                $(element).datetimepicker({
+                    minDate: Date.now()
+                });
+            }
+        };
+    });
+
+    
+
+
+})(angular.module('BikeRental'));
